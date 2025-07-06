1399084202da6abb7aa8007c3b076c241ae843d2
shivamsingh0169 <krshivam0169@gmail.com>
2025-07-06 19:34:40 +0530

    render3

--- server/server.js
+++ server/server.js
@@ -56,6 +56,6 @@ await connectDB();
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
 
-}
+
 // Export server for vercal
 export default server;
