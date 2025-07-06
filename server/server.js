d8284329065a828d3b3cdd627b49d3c6b2a72ed4
shivamsingh0169 <krshivam0169@gmail.com>
2025-07-06 19:33:00 +0530

    render 2

--- server/server.js
+++ server/server.js
@@ -53,9 +53,9 @@ app.use('/api/messages',messageRouter)
 await connectDB();
 
 
-if(process.env.NODE_ENV !=="production"){
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT,()=>console.log("Server is running on PORT: " +PORT));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+
 }
 // Export server for vercal
 export default server;
